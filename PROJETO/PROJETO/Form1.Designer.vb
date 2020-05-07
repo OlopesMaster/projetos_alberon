898d4868a27feea03a66d5462ba47ea7851a2fbb
OlopesMaster <62860227+OlopesMaster@users.noreply.github.com>
2020-05-07 19:01:27 -0300

    1

---
+++ PROJETO/PROJETO/Form1.Designer.vb
@@ -0,0 +1,297 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Projeto1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Projeto1))
+        Me.lblcodop = New System.Windows.Forms.Label()
+        Me.lblnpec = New System.Windows.Forms.Label()
+        Me.lblsex = New System.Windows.Forms.Label()
+        Me.txtcodop = New System.Windows.Forms.TextBox()
+        Me.txtpec = New System.Windows.Forms.TextBox()
+        Me.cmdcalc = New System.Windows.Forms.Button()
+        Me.lblsm = New System.Windows.Forms.Label()
+        Me.txtsm = New System.Windows.Forms.TextBox()
+        Me.lbls = New System.Windows.Forms.Label()
+        Me.lblsaln = New System.Windows.Forms.Label()
+        Me.lblsum = New System.Windows.Forms.Label()
+        Me.lblss = New System.Windows.Forms.Label()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.lblmpecmA = New System.Windows.Forms.Label()
+        Me.lblmpechA = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblmpecmB = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblmpecmC = New System.Windows.Forms.Label()
+        Me.lblmpechB = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblmpechC = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lbl5 = New System.Windows.Forms.Label()
+        Me.lbl6 = New System.Windows.Forms.Label()
+        Me.lblcdm = New System.Windows.Forms.Label()
+        Me.lblcdmn = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lbl0 = New System.Windows.Forms.Label()
+        Me.txtsexo = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblcodop
+        '
+        resources.ApplyResources(Me.lblcodop, "lblcodop")
+        Me.lblcodop.Name = "lblcodop"
+        '
+        'lblnpec
+        '
+        resources.ApplyResources(Me.lblnpec, "lblnpec")
+        Me.lblnpec.Name = "lblnpec"
+        '
+        'lblsex
+        '
+        resources.ApplyResources(Me.lblsex, "lblsex")
+        Me.lblsex.Name = "lblsex"
+        '
+        'txtcodop
+        '
+        resources.ApplyResources(Me.txtcodop, "txtcodop")
+        Me.txtcodop.Name = "txtcodop"
+        '
+        'txtpec
+        '
+        resources.ApplyResources(Me.txtpec, "txtpec")
+        Me.txtpec.Name = "txtpec"
+        '
+        'cmdcalc
+        '
+        Me.cmdcalc.BackColor = System.Drawing.Color.Red
+        resources.ApplyResources(Me.cmdcalc, "cmdcalc")
+        Me.cmdcalc.Name = "cmdcalc"
+        Me.cmdcalc.UseVisualStyleBackColor = False
+        '
+        'lblsm
+        '
+        resources.ApplyResources(Me.lblsm, "lblsm")
+        Me.lblsm.Name = "lblsm"
+        '
+        'txtsm
+        '
+        resources.ApplyResources(Me.txtsm, "txtsm")
+        Me.txtsm.Name = "txtsm"
+        '
+        'lbls
+        '
+        resources.ApplyResources(Me.lbls, "lbls")
+        Me.lbls.Name = "lbls"
+        '
+        'lblsaln
+        '
+        resources.ApplyResources(Me.lblsaln, "lblsaln")
+        Me.lblsaln.Name = "lblsaln"
+        '
+        'lblsum
+        '
+        resources.ApplyResources(Me.lblsum, "lblsum")
+        Me.lblsum.Name = "lblsum"
+        '
+        'lblss
+        '
+        resources.ApplyResources(Me.lblss, "lblss")
+        Me.lblss.Name = "lblss"
+        '
+        'lbl1
+        '
+        resources.ApplyResources(Me.lbl1, "lbl1")
+        Me.lbl1.Name = "lbl1"
+        '
+        'lbl2
+        '
+        resources.ApplyResources(Me.lbl2, "lbl2")
+        Me.lbl2.Name = "lbl2"
+        '
+        'lbl3
+        '
+        resources.ApplyResources(Me.lbl3, "lbl3")
+        Me.lbl3.Name = "lbl3"
+        '
+        'lblmpecmA
+        '
+        resources.ApplyResources(Me.lblmpecmA, "lblmpecmA")
+        Me.lblmpecmA.Name = "lblmpecmA"
+        '
+        'lblmpechA
+        '
+        resources.ApplyResources(Me.lblmpechA, "lblmpechA")
+        Me.lblmpechA.Name = "lblmpechA"
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'lblmpecmB
+        '
+        resources.ApplyResources(Me.lblmpecmB, "lblmpecmB")
+        Me.lblmpecmB.Name = "lblmpecmB"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'lblmpecmC
+        '
+        resources.ApplyResources(Me.lblmpecmC, "lblmpecmC")
+        Me.lblmpecmC.Name = "lblmpecmC"
+        '
+        'lblmpechB
+        '
+        resources.ApplyResources(Me.lblmpechB, "lblmpechB")
+        Me.lblmpechB.Name = "lblmpechB"
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Name = "Label6"
+        '
+        'lblmpechC
+        '
+        resources.ApplyResources(Me.lblmpechC, "lblmpechC")
+        Me.lblmpechC.Name = "lblmpechC"
+        '
+        'Label8
+        '
+        resources.ApplyResources(Me.Label8, "Label8")
+        Me.Label8.Name = "Label8"
+        '
+        'lbl5
+        '
+        resources.ApplyResources(Me.lbl5, "lbl5")
+        Me.lbl5.Name = "lbl5"
+        '
+        'lbl6
+        '
+        resources.ApplyResources(Me.lbl6, "lbl6")
+        Me.lbl6.Name = "lbl6"
+        '
+        'lblcdm
+        '
+        resources.ApplyResources(Me.lblcdm, "lblcdm")
+        Me.lblcdm.Name = "lblcdm"
+        '
+        'lblcdmn
+        '
+        resources.ApplyResources(Me.lblcdmn, "lblcdmn")
+        Me.lblcdmn.Name = "lblcdmn"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'lbl0
+        '
+        resources.ApplyResources(Me.lbl0, "lbl0")
+        Me.lbl0.BackColor = System.Drawing.Color.White
+        Me.lbl0.Name = "lbl0"
+        '
+        'txtsexo
+        '
+        resources.ApplyResources(Me.txtsexo, "txtsexo")
+        Me.txtsexo.Name = "txtsexo"
+        '
+        'Projeto1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.txtsexo)
+        Me.Controls.Add(Me.lbl0)
+        Me.Controls.Add(Me.lblcdmn)
+        Me.Controls.Add(Me.lblcdm)
+        Me.Controls.Add(Me.lbl6)
+        Me.Controls.Add(Me.lbl5)
+        Me.Controls.Add(Me.lblmpechC)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.lblmpechB)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblmpecmC)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblmpecmB)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblmpechA)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.lblmpecmA)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.lblss)
+        Me.Controls.Add(Me.lblsum)
+        Me.Controls.Add(Me.lblsaln)
+        Me.Controls.Add(Me.lbls)
+        Me.Controls.Add(Me.txtsm)
+        Me.Controls.Add(Me.lblsm)
+        Me.Controls.Add(Me.cmdcalc)
+        Me.Controls.Add(Me.txtpec)
+        Me.Controls.Add(Me.txtcodop)
+        Me.Controls.Add(Me.lblsex)
+        Me.Controls.Add(Me.lblnpec)
+        Me.Controls.Add(Me.lblcodop)
+        Me.Name = "Projeto1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblcodop As System.Windows.Forms.Label
+    Friend WithEvents lblnpec As System.Windows.Forms.Label
+    Friend WithEvents lblsex As System.Windows.Forms.Label
+    Friend WithEvents txtcodop As System.Windows.Forms.TextBox
+    Friend WithEvents txtpec As System.Windows.Forms.TextBox
+    Friend WithEvents cmdcalc As System.Windows.Forms.Button
+    Friend WithEvents lblsm As System.Windows.Forms.Label
+    Friend WithEvents txtsm As System.Windows.Forms.TextBox
+    Friend WithEvents lbls As System.Windows.Forms.Label
+    Friend WithEvents lblsaln As System.Windows.Forms.Label
+    Friend WithEvents lblsum As System.Windows.Forms.Label
+    Friend WithEvents lblss As System.Windows.Forms.Label
+    Friend WithEvents lbl1 As System.Windows.Forms.Label
+    Friend WithEvents lbl2 As System.Windows.Forms.Label
+    Friend WithEvents lbl3 As System.Windows.Forms.Label
+    Friend WithEvents lblmpecmA As System.Windows.Forms.Label
+    Friend WithEvents lblmpechA As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblmpecmB As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblmpecmC As System.Windows.Forms.Label
+    Friend WithEvents lblmpechB As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents lblmpechC As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents lbl5 As System.Windows.Forms.Label
+    Friend WithEvents lbl6 As System.Windows.Forms.Label
+    Friend WithEvents lblcdm As System.Windows.Forms.Label
+    Friend WithEvents lblcdmn As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lbl0 As System.Windows.Forms.Label
+    Friend WithEvents txtsexo As System.Windows.Forms.TextBox
+
+End Class
---
+++ PROJETO/PROJETO/Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class Projeto1
+    Dim nvsal, totsum, contma, contmb, contmc, mediamasa, mediamasb, mediamasc, contfa, contfb, contfc, mediafea, mediafeb, mediafec, maiorsal, menorsal, cont, smpam, smpbm, smpcm, smpaf, smpbf, smpcf As Double
+    Dim sexo As String
+    Private Sub cmdcalc_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdcalc.Click
+
+        cont += 1
+        'verifica se é da categoria A
+        sexo = txtsexo.Text
+        If CDbl(txtpec.Text) <= 30 Then
+            'faz a media de peça masculina de A
+
+            Select sexo
+                Case "m"
+
+                    contma += 1
+                    smpam += CDbl(txtpec.Text)
+                    mediamasa = smpam / contma
+                    lblmpechA.Text = CStr(mediamasa)
+
+        'faz a media de peça feminina de A
+            case "f"
+                    contfa += 1
+                    smpaf += CDbl(txtpec.Text)
+                    mediafea = smpaf / contfa
+                    lblmpecmA.Text = CStr(mediafea)
+            End select
+        nvsal = CDbl(txtsm.Text)
+        Else
+
+        'verifica se é da categoria B
+        If CDbl(txtpec.Text) < 35 And CDbl(txtpec.Text) > 30 Then
+            'faz a media de peça masculina de B
+
+            Select Case sexo
+                Case "m"
+                    contmb += 1
+                    smpbm += CDbl(txtpec.Text)
+                    mediamasb = smpbm / contmb
+                    lblmpechB.Text = CStr(mediamasb)
+
+                    'faz a media de peça feminina de B
+                Case "f"
+                    contfb += 1
+                    smpbf += CDbl(txtpec.Text)
+                    mediafeb = smpbf / contfb
+                    lblmpecmB.Text = CStr(mediafeb)
+            End Select
+            nvsal = CDbl(txtsm.Text) + (CDbl(txtpec.Text) - 30) * (CDbl(txtsm.Text) * 3 / 100)
+
+
+            'verifica se é da categoria C
+        Else
+
+
+            Select Case sexo
+                Case "m"
+                    contmc += 1
+                    smpcm += CDbl(txtpec.Text)
+                    mediamasc = smpcm / contmc
+                    lblmpechC.Text = CStr(mediamasc)
+
+                    'faz a media de peça feminina de C
+                Case "f"
+                    contfc += 1
+                    smpcf += CDbl(txtpec.Text)
+                    mediafec = smpcf / contfc
+                    lblmpecmC.Text = CStr(mediafec)
+            End Select
+            nvsal = CDbl(txtsm.Text) + (CDbl(txtpec.Text) - 30) * (CDbl(txtsm.Text) * 5 / 100)
+        End If
+        End If
+        'fim das analises de categoria
+
+        'serve para contralar o codigo do menor salario
+        If cont = 1 Then
+            menorsal = nvsal
+        End If
+
+        'verifica se o novo salario é maior
+        If nvsal > maiorsal Then
+            maiorsal = nvsal
+            lblcdm.Text = txtcodop.Text
+        End If
+        'verifica se o novo salario é maior
+        If menorsal >= nvsal Then
+            menorsal = nvsal
+            lblcdmn.Text = txtcodop.Text
+        End If
+
+        'manda as informaçoes para o app
+        lblsaln.Text = CStr(nvsal)
+        totsum += nvsal
+        lblss.Text = CStr(totsum)
+
+    End Sub
+
+    Private Sub Projeto1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
